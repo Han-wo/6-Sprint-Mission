f25856c08768e5ea11803b7915ea514adddff8bb
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2024-07-19 16:01:33 +0900

    feat : 전체상품 베스트 상품페이지 제작

--- next.config.js
+++ next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
         port: '',
         pathname: '/Sprint_Mission/user/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/...',
+      },
     ],
   },
 };
